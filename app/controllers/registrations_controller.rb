a3a155caa718a739a2e7efdcbe46aa7d836eb337
emmanuelkamala <emmanuelkamala86@gmail.com>
2022-03-07 16:51:44 +0300

    Correct sign up error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(user)
-    links_path
+    '/links'
   end
 
   def after_sign_in_path_for(user)
-    redirect_to links_path
+    links_path
   end
 end
