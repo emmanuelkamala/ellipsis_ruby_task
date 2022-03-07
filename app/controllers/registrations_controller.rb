747fe5e6d4afea1179e0b90f0f740c8c24817241
emmanuelkamala <emmanuelkamala86@gmail.com>
2022-03-07 18:15:12 +0300

    Add validation to url

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(user)
-    '/links'
+    '/links/new'
   end
 
   def after_sign_in_path_for(user)
